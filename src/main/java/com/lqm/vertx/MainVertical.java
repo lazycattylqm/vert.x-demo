53d08791d4afb6e08f30f32d9f8b2d7036d17264
LiQiming <leeqiming@163.com>
2022-05-10 19:30:02 +0800

    body handler

--- src/main/java/com/lqm/vertx/MainVertical.java
+++ src/main/java/com/lqm/vertx/MainVertical.java
@@ -2,8 +2,15 @@ package com.lqm.vertx;
 
 import com.lqm.vertx.model.vo.TempResponse;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
 import io.vertx.core.Promise;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
+import lombok.extern.slf4j.Slf4j;
 
 public class MainVertical extends AbstractVerticle {
     @Override
@@ -11,6 +18,11 @@ public class MainVertical extends AbstractVerticle {
         Router router = Router.router(vertx);
         router.route("/").handler(context -> context.json(new TempResponse()));
         router.route("/get").handler(context -> context.response().end("get"));
+        router.route(HttpMethod.POST, "/post").handler(BodyHandler.create()).handler(context -> {
+            JsonObject bodyAsJson = context.getBodyAsJson();
+            TempResponse tempResponse = bodyAsJson.mapTo(TempResponse.class);
+            context.json(tempResponse);
+        });
         vertx.createHttpServer().requestHandler(router).listen(8888).onSuccess(server -> {
             System.out.println("Http server started on port " + server.actualPort());
         });
