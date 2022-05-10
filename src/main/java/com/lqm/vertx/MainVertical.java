467b3a7977d45a4ef88d77eb13b219ea21250f95
LiQiming <leeqiming@163.com>
2022-05-10 23:44:34 +0800

    log handler

--- src/main/java/com/lqm/vertx/MainVertical.java
+++ src/main/java/com/lqm/vertx/MainVertical.java
@@ -12,9 +12,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
+@Slf4j
 public class MainVertical extends AbstractVerticle {
-    private static final Logger log = LoggerFactory.getLogger(MainVertical.class);
+//    private static final Logger log = LoggerFactory.getLogger(MainVertical.class);
     @Override
     public void start(Promise<Void> startPromise) throws Exception {
         Router router = Router.router(vertx);
