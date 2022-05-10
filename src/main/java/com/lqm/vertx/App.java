e80a92d09973dd24299a1109275adfc5333a9810
LiQiming <leeqiming@163.com>
2022-05-10 23:44:02 +0800

    log handler

--- src/main/java/com/lqm/vertx/App.java
+++ src/main/java/com/lqm/vertx/App.java
@@ -1,7 +1,11 @@
 package com.lqm.vertx;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App {
-    public static void main(String[] args) throws Exception {
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+    public static void main(String[] args) throws Exception {;
         new MainVertical().start();
 
     }
--- src/main/java/com/lqm/vertx/MainVertical.java
+++ src/main/java/com/lqm/vertx/MainVertical.java
@@ -2,25 +2,26 @@ package com.lqm.vertx;
 
 import com.lqm.vertx.model.vo.TempResponse;
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Future;
 import io.vertx.core.Promise;
-import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpMethod;
-import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.handler.LoggerHandler;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@Slf4j
+
 public class MainVertical extends AbstractVerticle {
+    private static final Logger log = LoggerFactory.getLogger(MainVertical.class);
     @Override
     public void start(Promise<Void> startPromise) throws Exception {
         Router router = Router.router(vertx);
         router.route("/").handler(context -> context.json(new TempResponse()));
         router.route("/get").handler(context -> context.response().end("get"));
         router.route(HttpMethod.POST, "/post").handler(LoggerHandler.create()).handler(BodyHandler.create()).handler(context -> {
+            log.info("path");
             JsonObject bodyAsJson = context.getBodyAsJson();
             TempResponse tempResponse = bodyAsJson.mapTo(TempResponse.class);
             context.json(tempResponse);
