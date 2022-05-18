0f6a95956c2f95a34beb76a69b2bdfef30d94159
LiQiming <leeqiming@163.com>
2022-05-18 23:38:05 +0800

    query params

--- src/main/java/com/lqm/vertx/base/BaseRouterController.java
+++ src/main/java/com/lqm/vertx/base/BaseRouterController.java
@@ -2,7 +2,9 @@ package com.lqm.vertx.base;
 
 import com.lqm.vertx.model.vo.TempResponse;
 import io.vertx.ext.web.Router;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class BaseRouterController {
     public static void registerRouter(Router router) {
         router.route("/temp/reg/").handler(context->{
@@ -10,5 +12,10 @@ public class BaseRouterController {
         });
 
         router.route("/").handler(context -> context.json(new TempResponse()));
+
+        router.route("/with/query/params").blockingHandler(context->{
+            log.info("params {}", context.queryParams());
+            context.end( "ok");
+        });
     }
 }
